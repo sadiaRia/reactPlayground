14750a22b2e51c62d93118f9bc4789da47ddc46d
sadia <sadia@techhivebd.com>
2020-05-02 00:59:44 +0600

    handle form submit

--- formik/src/components/Form.js
+++ formik/src/components/Form.js
@@ -6,18 +6,23 @@ function Form() {
       name: '',
       email: '',
       channel: ''
+    },
+    onSubmit: value => {
+      console.log('formik value', value);
     }
   })
-  console.log('formik values', formik.values);
+  // console.log('formik values', formik.values);
   return (
     <div>
-      <label htmlFor='name'>Name</label>
-      <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
-      <label htmlFor='email'>E-mail</label>
-      <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
-      <label htmlFor='channel'>Channel</label>
-      <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
-      <button>Submit</button>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor='name'>Name</label>
+        <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
+        <label htmlFor='email'>E-mail</label>
+        <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
+        <label htmlFor='channel'>Channel</label>
+        <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
