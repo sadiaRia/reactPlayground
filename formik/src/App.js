32d4efbd208206f5d33679b5c215da359614757c
sadia <sadia@techhivebd.com>
2020-05-01 23:30:59 +0600

    add simple form

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world :)</h1>
+      <Form/>
     </div>
   );
 }
---
+++ formik/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+      <label htmlFor='name'>Name</label>
+      <input type='text' id='name' name='name' />
+      <label htmlFor='email'>E-mail</label>
+      <input type='email' id='email' name='email' />
+      <label htmlFor='channel'>Channel</label>
+      <input type='text' id='channel' name='channel' />
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Form
