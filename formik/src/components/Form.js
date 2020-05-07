53d4f49d3f9fad0c3d890e5dc904845db5182306
sadia <sadia@techhivebd.com>
2020-05-07 22:40:12 +0600

    add handle blur for getting visited field

--- formik/src/components/Form.js
+++ formik/src/components/Form.js
@@ -35,25 +35,26 @@ function Form() {
   })
 
   // console.log('formik values', formik.values);
-  console.log('formik error', formik.errors);
+  // console.log('formik error', formik.errors);
+  console.log('visited field', formik.touched);
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
-          <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
+          <input type='text' id='name' name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
           {formik.errors.name && <div className='error'>{formik.errors.name}</div>}
         </div>
         <div className='form-control'>
 
           <label htmlFor='email'>E-mail</label>
-          <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
+          <input type='email' id='email' name='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
           {formik.errors.email && <div className='error'>{formik.errors.email}</div>}
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
-          <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
+          <input type='text' id='channel' name='channel' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.channel} />
           {formik.errors.channel && <div className='error'>{formik.errors.channel}</div>}
         </div>
         <button type="submit">Submit</button>
