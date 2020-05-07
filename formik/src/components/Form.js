22874fa08cc76f1160d4fad1bce17c3c99aa3a82
sadia <sadia@techhivebd.com>
2020-05-07 22:47:52 +0600

    iproving validation

--- formik/src/components/Form.js
+++ formik/src/components/Form.js
@@ -44,18 +44,18 @@ function Form() {
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
           <input type='text' id='name' name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
-          {formik.errors.name && <div className='error'>{formik.errors.name}</div>}
+          {formik.touched.name && formik.errors.name && <div className='error'>{formik.errors.name}</div>}
         </div>
         <div className='form-control'>
 
           <label htmlFor='email'>E-mail</label>
           <input type='email' id='email' name='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
-          {formik.errors.email && <div className='error'>{formik.errors.email}</div>}
+          {formik.touched.email && formik.errors.email && <div className='error'>{formik.errors.email}</div>}
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
           <input type='text' id='channel' name='channel' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.channel} />
-          {formik.errors.channel && <div className='error'>{formik.errors.channel}</div>}
+          {formik.touched.channel && formik.errors.channel && <div className='error'>{formik.errors.channel}</div>}
         </div>
         <button type="submit">Submit</button>
       </form>
