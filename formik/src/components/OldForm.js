847e5864bb41bd8231df780fa2b6124e6e3aa1d9
sadia <sadia@techhivebd.com>
2020-05-15 22:53:42 +0600

    reduce boilerplate

--- formik/src/components/OldForm.js
+++ formik/src/components/OldForm.js
@@ -12,19 +12,37 @@ const onSubmit = values => {
   console.log('formik value', values);
 };
 
+const validate = values => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email format';
+  }
+  if (!values.channel) {
+    errors.channel = 'Required';
+  }
+  return errors;
+}
 const validationSchema = yup.object({
   name: yup.string().required('This field is required'),
   email: yup.string().email('Invalid email address').required('This field is required'),
   channel: yup.string().required('This field is required'),
 })
 
-function Form() {
+function OldForm() {
   const formik = useFormik({
     initialValues,
     onSubmit,
+    // validate
     validationSchema
   })
 
+  // console.log('formik values', formik.values);
+  // console.log('formik error', formik.errors);
   console.log('visited field', formik.touched);
 
   return (
@@ -32,18 +50,18 @@ function Form() {
       <form onSubmit={formik.handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
-          <input type='text' id='name' name='name' {...formik.getFieldProps('name')} />
+          <input type='text' id='name' name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
           {formik.touched.name && formik.errors.name && <div className='error'>{formik.errors.name}</div>}
         </div>
         <div className='form-control'>
 
           <label htmlFor='email'>E-mail</label>
-          <input type='email' id='email' name='email' {...formik.getFieldProps('email')} />
+          <input type='email' id='email' name='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
           {formik.touched.email && formik.errors.email && <div className='error'>{formik.errors.email}</div>}
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
-          <input type='text' id='channel' name='channel' {...formik.getFieldProps('channel')} />
+          <input type='text' id='channel' name='channel' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.channel} />
           {formik.touched.channel && formik.errors.channel && <div className='error'>{formik.errors.channel}</div>}
         </div>
         <button type="submit">Submit</button>
@@ -52,4 +70,4 @@ function Form() {
   )
 }
 
-export default Form
+export default OldForm
