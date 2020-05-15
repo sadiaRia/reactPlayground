ccff764470691b0d9ff1667b6acb16ded5ef7adc
sadia <sadia@techhivebd.com>
2020-05-15 22:59:37 +0600

    import oldform

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'
 import Form from './components/Form'
+import OldForm from './components/OldForm'
 
 function App() {
   return (
     <div className="App">
+      {/* <OldForm /> */}
       <Form/>
     </div>
   );
